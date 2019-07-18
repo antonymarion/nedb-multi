58dcdb9dd60deb5cdfd5d599303e8df5ce3bd0e0
antonymarion <marionantony@gmail.com>
2019-07-18 19:24:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const axon = require('axon');
 
 const handler = require('./lib/handler');
 
-const port = Number(process.env.NEDB_MULTI_PORT) || Number(process.argv[2]);
+const port = Number(process.env.NEDB_MULTI_PORT  || 5151) || Number(process.argv[2]);
 const dbsMap = new Map();
 const repSocket = axon.socket('rep');
 const messagesHandler = handler.create(dbsMap);
