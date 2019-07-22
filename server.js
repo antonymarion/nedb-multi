2ac26dd268a0dd65d5e48072ef1e6a255f1225fe
antonymarion <marionantony@gmail.com>
2019-07-22 11:44:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,35 @@ const axon = require('axon');
 
 const handler = require('./lib/handler');
 
-const port = Number(process.env.NEDB_MULTI_PORT  || 5151) || Number(process.argv[2]);
+const ps = require("ps-node");
+
+
+const port = Number(process.env.NEDB_MULTI_PORT || 5151) || Number(process.argv[2]);
 const dbsMap = new Map();
 const repSocket = axon.socket('rep');
 const messagesHandler = handler.create(dbsMap);
 
+// do nothing
 repSocket.bind(port);
 repSocket.on('message', messagesHandler);
 
-exports.endConnection = function(){
+exports.endConnection = function (callback) {
+
+    // A simple pid lookup
+    const pid = parseInt(repSocket.settings.identity);
+
     repSocket.close();
-    process.exit(0);
+
+    // process.exit();
+    // return callback();
+    ps.kill(pid,
+        function (err) {
+            if (err) {
+                return callback(err);
+            }
+            return callback();
+        });
+
 }
+
 
